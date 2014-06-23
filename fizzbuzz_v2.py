0093a302c5f199f147bcc542cce9ef67f6c68498
skn <skn@methodpark.de>
2014-06-23 08:32:52 +0200

    add files for fizzbuzz kata

---
+++ fizzbuzz_v2.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+import sys, getopt
+
+def fizzbuzz(n):
+ 
+    if n % 3 == 0 and n % 5 == 0:
+        return 'FizzBuzz'
+    elif n % 3 == 0:
+        return 'Fizz'
+    elif n % 5 == 0:
+        return 'Buzz'
+    else:
+        return str(n)
+ 
+def main(argv):
+   start = 1
+   end = 0
+   try:
+      opts, args = getopt.getopt(argv,"hs:e:")
+   except getopt.GetoptError:
+      print 'fizzbuzz.py -s <startvalue> -e <endvalue>'
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print 'fizzbuzz.py -s <startvalue> -e <endvalue>'
+         sys.exit()
+      elif opt in ("-s"):
+         start = int(arg)
+      elif opt in ("-e"):
+         end = int(arg)
+   if end < start:
+   	  print 'end value missing or too small!'
+   	  print 'fizzbuzz.py -s <startvalue> -e <endvalue>'
+   	  sys.exit()      
+   print "\n".join(fizzbuzz(n) for n in xrange(start, end))
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
+
+
