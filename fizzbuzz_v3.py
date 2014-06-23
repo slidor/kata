849247a52fdaa397e8ec7144a248d3b0eefd3d17
Sebastian Kern <sebastian.kern@methodpark.de>
2014-06-23 12:06:28 +0200

    add v3 as final version for kata

---
+++ fizzbuzz_v3.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+import sys, getopt
+
+def fizzbuzz(n):
+ 
+    if n % 3 == 0 and n % 5 == 0:
+        return 'FizzBuzz'
+    elif n % 3 == 0:
+        return 'Fizz'
+    elif n % 5 == 0:
+        return 'Buzz'
+    else:
+        return str(n)
+ 
+def main(argv):
+   start = 1
+   end = 100
+   try:
+      opts, args = getopt.getopt(argv,"hs:e:")
+   except getopt.GetoptError:
+      print 'fizzbuzz.py -s <startvalue> -e <endvalue>'
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print 'fizzbuzz.py -s <startvalue> -e <endvalue>'
+         sys.exit()
+      elif opt in ("-s"):
+         start = int(arg)
+      elif opt in ("-e"):
+         end = int(arg)     
+   print "\n".join(fizzbuzz(n) for n in xrange(start, end+1))
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
+
+
